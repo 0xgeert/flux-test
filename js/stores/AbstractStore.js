3f4049f8bf54b596139d8b42434b62459bf8ce9c
gbrits <gbrits@gmail.com>
2014-05-26 17:06:47 +0200

    formatting

--- js/stores/AbstractStore.js
+++ js/stores/AbstractStore.js
@@ -3,48 +3,48 @@
 /**
  *
  * GeneralStore
- * Subclass needs to implement: 
+ * Subclass needs to implement:
  * - callbackFN
  * - CHANGE_EVENT
  *
- * Sets 
+ * Sets
  */
 
 var _ = require("lodash");
 
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 
-var EventEmitter = require('events').EventEmitter; 
+var EventEmitter = require('events').EventEmitter;
 
 var merge = require('react/lib/merge');
 
 
 var AbstractStore = merge(EventEmitter.prototype, {
   init: function() {
-    if(this.callbackFN === undefined && this.actions === undefined){
+    if (this.callbackFN === undefined && this.actions === undefined) {
       throw new Error("Store should have 'callbackFN' or 'actions'  defined");
     }
 
-    if(this.callbackFN !== undefined && this.actions !== undefined){
+    if (this.callbackFN !== undefined && this.actions !== undefined) {
       throw new Error("Store cannot have both 'callbackFN' and 'actions'  defined");
     }
 
-     if(this.actions !== undefined && this.constants === undefined ){
+    if (this.actions !== undefined && this.constants === undefined) {
       throw new Error("Store should have 'constants' defined if 'actions' defined");
     }
 
-    if(this.CHANGE_EVENT === undefined){
+    if (this.CHANGE_EVENT === undefined) {
       throw new Error("Store should have 'CHANGE_EVENT'  defined");
     }
 
     //create 'callbackFN' from this.actions which is defined declaratively
-    if(this.actions !== undefined){
+    if (this.actions !== undefined) {
 
       var map = this.createMapFromActionDeclaration(this.actions);
 
       this.callbackFN = function(payload) {
         var action = payload.action;
-        if(map[action.actionType] !== undefined){
+        if (map[action.actionType] !== undefined) {
           map[action.actionType].fn.call(this, action);
         }
       };
@@ -56,8 +56,8 @@ var AbstractStore = merge(EventEmitter.prototype, {
 
   // dispatchIndex set by index
   //dispatchIndex: FN
-  
-   /**
+
+  /**
    * @param {function} callback
    */
   addChangeListener: function(callback) {
@@ -75,28 +75,28 @@ var AbstractStore = merge(EventEmitter.prototype, {
     this.emit(this.CHANGE_EVENT);
   },
 
-  createMapFromActionDeclaration: function(){
+  createMapFromActionDeclaration: function() {
 
     var that = this;
 
     //create map of actions to handlers. 
     //throw if action and/or handler is not found
     var map = {};
-    _.each(this.actions, function(obj,k){
+    _.each(this.actions, function(obj, k) {
       var lookupKey = that.constants[k];
-      if(lookupKey === undefined){
+      if (lookupKey === undefined) {
         throw new Error("action not found in store: " + k);
       }
       var fn;
-      if(_.isString(obj)){
+      if (_.isString(obj)) {
         fn = that[obj];
-        if(!_.isFunction(fn)){
+        if (!_.isFunction(fn)) {
           throw new Error("actionHandler by reference not found in store with ref: " + obj);
         }
         obj = {
           fn: fn
         };
-      }else if(_.isFunction(obj)){
+      } else if (_.isFunction(obj)) {
         obj = {
           fn: obj
         };
@@ -106,7 +106,7 @@ var AbstractStore = merge(EventEmitter.prototype, {
       //  fn: fn
       //}
       //
-      if(!_.isObject(obj)){
+      if (!_.isObject(obj)) {
         throw new Error("config error for actionHandler in store: " + k)
       }
 
@@ -118,12 +118,12 @@ var AbstractStore = merge(EventEmitter.prototype, {
       //{
       //  fn: function
       //}
-      if(_.isString(obj.fn)){
+      if (_.isString(obj.fn)) {
         fn = that[obj.fn];
-        if(!_.isFunction(fn)){
+        if (!_.isFunction(fn)) {
           throw new Error("actionHandler by reference not found in store with ref: " + obj.fn);
         }
-        obj.fn= fn;
+        obj.fn = fn;
       }
 
       map[lookupKey] = obj;
