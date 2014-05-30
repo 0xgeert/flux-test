59f6f1ebe9b2424f0731357ac756fd1b8a166383
gbrits <gbrits@gmail.com>
2014-05-30 13:17:05 +0200

    strange merge problem seems solved (going from optimistic -> master)

--- js/repositories/TodoRepository.js
+++ js/repositories/TodoRepository.js
@@ -1,8 +1,5 @@
 "use strict";
 
-//check perf diff
-var usePouch = true;
-
 var Promise = require('es6-promise').Promise;
 
 var _ = require("lodash");
@@ -11,56 +8,107 @@ var PouchDB = require('pouchdb');
 var dbPouch = new PouchDB('todos');
 var remoteCouch = false;
 
+
+// when pouch signals error -> repopulate in-mem todos
+// this effectively functions as a rollback 
+var rollbackAfterConflict = function(err){
+	console.log(err);
+	console.log("rolling back...");
+	todos = undefined; //clear cache
+	return db.allDocs({include_docs: true});
+};
+
 //simple implementation to check if bad performance is indeed due to pouchDB
-var todos = {};
-var dbInMem = {
-  allDocs: function(){
-    var result = {
-      rows : _.map(_.values(todos), function(todo){
-        return {doc: todo};
-      })
-    };
-    return Promise.resolve(result);
+var db = dbPouch;
+var todos;
+db = {
+
+  allDocs: function(opts){
+  	if(!todos){
+  		// console.log("remote lookup");
+  		return dbPouch.allDocs(opts).then(function(result){
+  			var docs = _.pluck(result.rows, "doc");
+  			todos = _.zipObject(_.pluck(docs, '_id'), docs);
+  			return result;
+  		});
+  	}else{
+  		// console.log("local lookup");
+  		var result = {
+	      rows : _.map(_.values(todos), function(todo){
+	        return {doc: todo};
+	      })
+	    };
+	    return Promise.resolve(result);
+  	}
   },
-  put: function(partial, id){
+
+  put: function(partial, id, rev){
+
+  	var doc;
     if(!id){
       //new
       todos[partial._id] = partial;
-      return Promise.resolve(partial);
+      doc = partial; 
     }else{
       //change existing
       var todo = todos[id];
       if(!todo){
         return Promise.reject("doc not found");
       }
-      todos[id] = _.merge(todo, partial);
-      return Promise.resolve(todos[id]);
+      doc = todos[id] = _.merge(todo, partial);
     }
+
+    console.log("local create/put done");
+    //in all cases update pouch
+    return dbPouch.put(doc, id, rev).then(function(result){
+    	console.log("remote create/put done");
+    	//update cache with changed/created rev and id which may not yet exist
+    	doc._rev = result.rev;
+    	doc.id = result.id;
+    })["catch"](rollbackAfterConflict);
   },
+  
+  //requires in-mem store to be populated!
   get: function(id){
+  	//always fetch on in-mem store
     return Promise.resolve(todos[id]);
   },
-  remove: function(id){
-    var todo =  todos[id];
+
+  remove: function(id, rev){
+  	if(rev === undefined){
+  		throw new Error("rev needs to be defined when calling store.remove");
+  	}
     delete todos[id];
-    return Promise.resolve(todo);
+    console.log("local remove done");
+    return dbPouch.remove(id, rev)["catch"](rollbackAfterConflict);
   },
+
   bulkDocs: function(docs){
     if(!docs.length){
       return Promise.resolve();
     }
     _.each(docs, function(doc){
       if(doc._deleted){
+      	//delete
         delete todos[doc.id];
       }else{
+      	//update
         todos[doc.id] = doc;
       }
     }); 
-    return Promise.resolve(docs);
+
+    console.log("local bulk done");
+    return dbPouch.bulkDocs(docs).then(function(result){
+    	//update cache with changed rev
+    	_.each(result, function(changedDoc){
+    		var doc = todos[changedDoc.id];
+    		if(doc){
+    			doc._rev = changedDoc.rev;
+    		}
+    	});
+    })["catch"](rollbackAfterConflict);
   }
 };
-
-var db = usePouch ? dbPouch : dbInMem;
 
 var TodoRepo = {
 
@@ -87,6 +135,7 @@ var TodoRepo = {
 	 *     updated.
 	 */
 	update: function(id, updates) {
+
 
 	  //Get the doc given id. This is needed because we need to specify a _rev of optimistic versioning.
 	  //Use the _rev and id to update the document.
@@ -145,9 +194,9 @@ var TodoRepo = {
 	},
 
 	getDocs: function(where){
-	  var start = Date.now();
+	  // var start = Date.now();
 	  return db.allDocs({include_docs: true}).then(function(result){
-	    console.log("getDocs took " + (Date.now() - start) + " millis");
+	    // console.log("getDocs took " + (Date.now() - start) + " millis");
 	    var docs =  _.map(_.pluck(result.rows, "doc"), function(doc){
 	      return _.merge(doc, {id: doc._id});
 	    });
