741983b49b9d90a4f8ad89fc4abdbf7fa084ea9c
gbrits <gbrits@gmail.com>
2014-06-05 21:29:21 +0200

    some weird linenumber offset. whatever

--- server/api/controllers/TodoController.js
+++ server/api/controllers/TodoController.js
@@ -1,14 +1,14 @@
 "use strict";
 
 /**
- * TodoController.js 
+ * TodoController.js
  *
  * Extended with `removeMulti` and `updateMulti`
  *
- * NOTE: this could prove generally useful. 
- * Suggested to add them upstream as part of CRUD-blueprints: 
+ * NOTE: this could prove generally useful.
+ * Suggested to add them upstream as part of CRUD-blueprints:
  * https://github.com/balderdashy/sails/issues/1805
- * 
+ *
  * @description ::
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
@@ -16,100 +16,105 @@
 var _ = require("lodash");
 module.exports = {
 
-	testSocket: function(req,res){
+	testSocket: function(req, res) {
 
-        if(req.isSocket){
+		if (req.isSocket) {
 
-          Todo.watch(req.socket);
-          console.log('Todo with socket id '+req.socket.id+' is now subscribed to the model class \'todo\'.');
+			Todo.watch(req.socket);
+			console.log('Todo with socket id ' + req.socket.id + ' is now subscribed to the model class \'todo\'.');
 
-        } else {
+		} else {
 
-          res.view();
+			res.view();
 
-        }
-    },
+		}
+	},
 
 	/**
 	 * payload: {
-	 * 	where: {}, 
+	 * 	where: {},
 	 * }
 	 *
 	 * or
-	 * 
+	 *
 	 * payload: {
-	 * 	ids: [], 
+	 * 	ids: [],
 	 * }
 	 *
 	 * @param  {[type]} req [description]
 	 * @param  {[type]} res [description]
 	 * @return {[type]}     [description]
 	 */
-	removeMulti: function(req, res){
+	removeMulti: function(req, res) {
 		var where = req.param('where'),
 			ids = req.param('ids');
 
-		if(!_.isObject(where) && !_.isObject(ids)){
+		if (!_.isObject(where) && !_.isObject(ids)) {
 			return res.badRequest("either 'where' or 'ids' should be defined");
 		}
-		if(_.isObject(where) && _.isObject(ids)){
+		if (_.isObject(where) && _.isObject(ids)) {
 			return res.badRequest("'where' and 'ids' were both defined. It's either or");
 		}
-		if(_.isObject(ids)){
+		if (_.isObject(ids)) {
 			where = {
 				id: ids
 			};
 		}
 		Todo.destroy(where).exec(function(err, todos) {
-		   if (err) {return res.serverError();}
+			if (err) {
+				return res.serverError();
+			}
 
-		   //let clients know what happened so they can sync up
-		   Todo.message(todos,{
-		   	moeha: "jaja"
-		   });
+			//let clients know what happened so they can sync up
+			//NOT WORKING ATM
+			// Todo.message(todos,{
+			// 	moeha: "jaja"
+			// });
 
-		   return res.json(todos);
+			return res.json(todos);
 		});
 	},
 
 	/**
 	 * payload: {
-	 * 	where: {}, 
+	 * 	where: {},
 	 * 	partial: {}
 	 * }
-	 * or: 
-	 * 
+	 * or:
+	 *
 	 * payload: {
-	 * 	ids: [], 
+	 * 	ids: [],
 	 * 	partial: {}
 	 * }
-	 * 
+	 *
 	 * @param  {[type]} req [description]
 	 * @param  {[type]} res [description]
 	 * @return {[type]}     [description]
 	 */
-	updateMulti: function(req, res){
+	updateMulti: function(req, res) {
 		var where = req.param('where'),
 			partial = req.param('partial'),
 			ids = req.param('ids');
 
-		if(!_.isObject(where) && !_.isObject(ids)){
+		if (!_.isObject(where) && !_.isObject(ids)) {
 			return res.badRequest("either 'where' or 'ids' should be defined");
 		}
-		if(_.isObject(where) && _.isObject(ids)){
+		if (_.isObject(where) && _.isObject(ids)) {
 			return res.badRequest("'where' and 'ids' were both defined. It's either or");
 		}
-		if(_.isObject(ids)){
+		if (_.isObject(ids)) {
 			where = {
 				id: ids
 			};
 		}
-		if(!_.isObject(partial)){
+		if (!_.isObject(partial)) {
 			return res.badRequest("'partial' should be defined");
 		}
 		Todo.update(where, partial).exec(function(err, todosUpdated) {
-		   if (err) {return res.serverError();}
-		   return res.json(todosUpdated);
+			if (err) {
+				return res.serverError();
+			}
+			return res.json(todosUpdated);
 		});
 	}
 
